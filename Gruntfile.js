1df2b9777a25458b12a90be857f3198b45bd05ef
Rubens Mariuzzo <rubens@mariuzzo.com>
2014-09-15 16:18:12 -0400

    banner configuration done.

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+
+        concat: {
+            dist: {
+                src: [
+                    'src/css/form.css',
+                    'src/css/listas.css',
+                    'src/css/tipografia.css'
+                ],
+                dest: 'dist/css/all.css'
+            }
+        },
+
+        cssmin: {
+            dist: {
+                src: 'dist/css/all.css',
+                dest: 'dist/css/all.concat.css'
+            },
+            options: {
+                banner: '/*! grunt-talk, v0.1.0 - Rubens Mariuzzo */'
+            }
+        },
+
+        watch: {
+            css: {
+                files: 'src/**/*.css',
+                tasks: ['concat', 'cssmin']
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+};
